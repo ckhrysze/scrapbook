14518d4c0797569c2d5f302516657aafd552e259
ckhrysze <ckhrysze@gmail.com>
2010-05-12 16:56:14 -0500

    Having some basic server communication working

--- fabfile.py
+++ fabfile.py
@@ -1,2 +1,15 @@
-def hello():
-    print("Hello world!")
+from __future__ import with_statement
+from fabric.api import *
+from fabric.contrib.console import confirm
+
+env.hosts = ['anaelise.net']
+
+def deploy():
+    pack()
+    put('tmp/scrapbook.tgz', '~/webapps/anaelise_app1')
+    with cd('~/webapps/anaelise_app1/scrapbook'):
+        run('ls ~/webapps/anaelise_app1')
+
+
+def pack():
+    local('tar czf tmp/scrapbook.tgz scrapbook', capture=False)
